fe6999f6202eff67cc495bdb68733d0dd4076880
Obadha <wobadha@gmail.com>
2017-06-02 11:52:32 +0300

    Implement Login UI.

---
+++ app/src/main/java/com/walt/baraza/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.walt.baraza;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by root on 6/2/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
